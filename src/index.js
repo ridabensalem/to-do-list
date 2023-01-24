071fff54aecbe35ae64d257a612a036c6a18546a
ridabensalem <ridasbnesalmas@gmail.com>
2023-01-24 18:26:26 +0100

    Fetching data form the object

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
-import _ from 'lodash';
+import './style.css';
+import fetchingData from './modules/fetch.js';
 
-function component() {
-    const element = document.createElement('div');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+fetchingData();
